dc471c1f32d3cf8f363175111786c02fcc548ac7
yinhang_sun <sunyinhang@gmail.com>
2023-05-08 21:23:15 -0700

    implement stay management

--- src/main/java/com/laioffer/staybooking/config/ElasticsearchConfig.java
+++ src/main/java/com/laioffer/staybooking/config/ElasticsearchConfig.java
@@ -8,6 +8,7 @@ import org.springframework.data.elasticsearch.client.ClientConfiguration;
 import org.springframework.data.elasticsearch.client.RestClients;
 import org.springframework.data.elasticsearch.config.AbstractElasticsearchConfiguration;
 
+// test login commit and push
 @Configuration
 public class ElasticsearchConfig extends AbstractElasticsearchConfiguration {
 
