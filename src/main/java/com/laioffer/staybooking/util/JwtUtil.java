df3ac69bd6a15c6d734999c4fc4535b03fbc37ec
yinhang_sun <sunyinhang@gmail.com>
2023-05-24 11:56:58 -0700

    implement token-based authentication

--- src/main/java/com/laioffer/staybooking/util/JwtUtil.java
+++ src/main/java/com/laioffer/staybooking/util/JwtUtil.java
@@ -1,5 +1,6 @@
 package com.laioffer.staybooking.util;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Value;
@@ -21,5 +22,21 @@ public class JwtUtil {
                 .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24))
                 .signWith(SignatureAlgorithm.HS256, secret)
                 .compact();
+    }
+
+    private Claims extractClaims(String token) {
+        return Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody();
+    }
+
+    public String extractUsername(String token) {
+        return extractClaims(token).getSubject();
+    }
+
+    public Date extractExpiration(String token) {
+        return extractClaims(token).getExpiration();
+    }
+
+    public Boolean validateToken(String token) {
+        return extractExpiration(token).after(new Date());
     }
 }
