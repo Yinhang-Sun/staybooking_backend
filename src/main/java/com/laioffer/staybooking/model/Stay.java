25a7ba1a534a4d37161170fabdb89fbdb9b1db75
yinhang_sun <sunyinhang@gmail.com>
2023-05-25 15:31:51 -0700

    implement stay management

---
+++ src/main/java/com/laioffer/staybooking/model/Stay.java
@@ -0,0 +1,10 @@
+package com.laioffer.staybooking.model;
+
+public class Stay {
+    private Long id;
+    private String name;
+    private String description;
+    private String address;
+    private int guestNumber;
+    private User host;
+}
