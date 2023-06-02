672092e25a9d06f7b1f157cace3461d19fb0fa8f
yinhang_sun <sunyinhang@gmail.com>
2023-06-02 13:08:54 -0700

    implement Geo-based Search

--- src/main/java/com/laioffer/staybooking/config/SecurityConfig.java
+++ src/main/java/com/laioffer/staybooking/config/SecurityConfig.java
@@ -65,4 +65,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     public AuthenticationManager authenticationManagerBean() throws Exception {
         return super.authenticationManagerBean();
     }
+
+
 }
