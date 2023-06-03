f68bdae4da22b52d50584c08b031cc52faa0cb64
yinhang_sun <sunyinhang@gmail.com>
2023-06-03 12:16:31 -0700

    implement reservation

---
+++ src/main/java/com/laioffer/staybooking/repository/ReservationRepository.java
@@ -0,0 +1,18 @@
+package com.laioffer.staybooking.repository;
+
+import com.laioffer.staybooking.model.Reservation;
+import com.laioffer.staybooking.model.Stay;
+import com.laioffer.staybooking.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+    List<Reservation> findByGuest(User user);
+
+    List<Reservation> findByStay(Stay stay);
+
+    Reservation findByIdAndGuest(Long id, User guest); // for deletion
+}
